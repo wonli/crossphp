0849ece14b8b2b0a4cd70327ee8484595a535d89
ideaa <ideaa@qq.com>
2016-12-30 16:23:27 +0800

    优化参数解析
    增加对命令的支持及合并注释配置

--- app/cli/controllers/Cli.php
+++ app/cli/controllers/Cli.php
@@ -14,29 +14,61 @@ use Cross\MVC\Controller;
  */
 abstract class Cli extends Controller
 {
-    protected $command = '';
+    /**
+     * @var string
+     */
+    protected $command;
 
     function __construct()
     {
         parent::__construct();
 
+        //处理注释配置中的参数
+        if (!empty($this->action_annotate['params'])) {
+            $params = $this->action_annotate['params'];
+        } else {
+            $params = array();
+        }
+
         //处理$argv传递过来的参数
         //params1=value1 params2=value2 ... paramsN=valueN
-        $params = array();
-        foreach ($this->params as $k => $p) {
-            if (strpos($p, '=') === false) {
-                if($k == 0) {
-                    $this->command = trim($p);
+        $i = 0;
+        foreach ($this->params as $p) {
+            if ((false === strpos($p, '=')) && $i == 0) {
+                $this->command = trim($p);
+            } elseif (!empty($p)) {
+                list($key, $value) = explode('=', $p);
+                if ($key && $value) {
+                    $params[trim(trim($key, '-'))] = trim($value);
                 }
-                continue;
             }
 
-            list($p_key, $p_value) = explode('=', $p);
-            if ($p_key && $p_value) {
-                $params[trim(trim($p_key, '-'))] = trim($p_value);
-            }
+            $i++;
         }
 
         $this->params = $params;
+    }
+
+    /**
+     * 输出消息并刷新输入输出缓存
+     *
+     * @param string $message
+     */
+    function flushMessage($message)
+    {
+        echo $message . PHP_EOL;
+        flush();
+        ob_flush();
+    }
+
+    /**
+     * @see flushMessage
+     *
+     * @param string $message
+     */
+    function endFlushMessage($message)
+    {
+        $this->flushMessage($message);
+        exit(0);
     }
 }
