f0df759ae52dc8cffe12dd2cb4e12ae462d0f087
ideaa <ideaa@qq.com>
2018-01-23 14:24:43 +0800

    *

--- app/admin/templates/adminlte/security/theme.tpl.php
+++ app/admin/templates/adminlte/security/theme.tpl.php
@@ -16,16 +16,17 @@ if (isset($data['default'])) {
         foreach ($themes as $name => $d) {
             $act = $this->url('security:profile', array('act' => 'setTheme', 'theme' => $name));
             ?>
-            <li style="float:left;width:25%;padding:5px 10px 5px 0">
-                <a href="<?php echo $act ?>" style="display:block;box-shadow:0 0 2px rgba(0,0,0,0.2)"
+            <li style="float:left;width:16.6666%;padding:5px 10px 5px 0">
+                <a href="<?php echo $act ?>"
+                   style="display:block;border:1px solid #cecece;box-shadow:-1px 1px 2px 0 rgba(0,0,0,0.2)"
                    class="clearfix full-opacity-hover">
                     <div>
-                        <span style="display:block;width:20%;float:left;height:12px;background:<?php echo $d['tlc'] ?> !important"></span>
-                        <span style="display:block;width:80%;float:left;height:12px;background:<?php echo $d['trc'] ?> !important"></span>
+                        <span style="display:block;width:20%;float:left;height:10px;background:<?php echo $d['tlc'] ?> !important"></span>
+                        <span style="display:block;width:80%;float:left;height:10px;background:<?php echo $d['trc'] ?> !important"></span>
                     </div>
                     <div>
-                        <span style="display:block;width:20%;float:left;height:25px;background:<?php echo $d['lc'] ?> !important"></span>
-                        <span style="display:block;width:80%;float:left;height:25px;background:#f4f5f7"></span>
+                        <span style="display:block;width:20%;float:left;height:20px;background:<?php echo $d['lc'] ?> !important"></span>
+                        <span style="display:block;width:80%;float:left;height:20px;background:#f4f5f7"></span>
                     </div>
                 </a>
             </li>
