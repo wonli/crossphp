d92ecf7474e251fc10f80a8a016d1195e0cba722
ideaa <ideaa@qq.com>
2018-07-20 10:28:27 +0800

    精简命令行模式下的调用方式

---
+++ cp
@@ -0,0 +1,3 @@
+<?php
+require __DIR__ . '/crossboot.php';
+Cross\Core\Delegate::loadApp('cli')->cliRun();
--- tools/cli/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @author wonli <wonli@live.com>
- * skeleton
- */
-require __DIR__ . '/../../crossboot.php';
-Cross\Core\Delegate::loadApp('cli')->cliRun();
