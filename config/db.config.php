2080350cde4e10807b737dffe4f64c86bf5ad7a7
wonli <wonli@live.com>
2020-05-29 17:50:11 +0800

    *

--- config/db.config.php
+++ config/db.config.php
@@ -2,24 +2,24 @@
 /**
  * mysql
  */
-$mysql_link = array(
+$mysql_link = [
     'host' => '127.0.0.1',
     'port' => '3306',
     'user' => 'root',
     'pass' => '123456',
     'prefix' => '',
     'charset' => 'utf8',
-);
+];
 
 /**
  * redis
  */
-$redis_link = array(
+$redis_link = [
     'host' => '127.0.0.1',
     'port' => 6379,
     'pass' => '',
     'timeout' => 2.5
-);
+];
 
 #默认数据库配置
 $db = $mysql_link;
