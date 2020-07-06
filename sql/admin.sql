8736c3123cdc28770aa5028d504c3db0f408ce72
wonli <wonli@live.com>
2020-07-06 16:24:53 +0800

    文档数据结构

--- sql/admin.sql
+++ sql/admin.sql
@@ -118,23 +118,33 @@ CREATE TABLE IF NOT EXISTS `cpa_doc` (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS `cpa_doc_data` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `u` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `doc_id` int(11) NOT NULL DEFAULT 0,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `value` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `cpa_doc_user` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `u` VARCHAR(32) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+  `doc_id` INT(10) NOT NULL DEFAULT '0',
+  `name` VARCHAR(128) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+  `value` TEXT NOT NULL COLLATE 'utf8_general_ci',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `u`(`u`) USING BTREE,
-  INDEX `name`(`name`) USING BTREE
+  INDEX `u` (`u`) USING BTREE,
+  INDEX `name` (`name`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE IF NOT EXISTS `cpa_doc_cache`  (
-  `cache_id` int(11) NOT NULL AUTO_INCREMENT,
-  `doc_id` int(11) NULL DEFAULT NULL,
-  `api_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
-  `api_response` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `cache_at` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`cache_id`) USING BTREE,
-  UNIQUE INDEX `doc_api_path_uindex`(`doc_id`, `api_path`) USING BTREE
+CREATE TABLE IF NOT EXISTS `cpa_doc_data`  (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `doc_id` INT(10) UNSIGNED NOT NULL,
+  `enable_mock` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '0,关闭mock 1,开启mock',
+  `global_params` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' COMMENT '全局参数是否生效',
+  `group_key` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '分类（类名）' COLLATE 'utf8_general_ci',
+  `group_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '分类名称' COLLATE 'utf8_general_ci',
+  `api_path` VARCHAR(128) NOT NULL COLLATE 'utf8_general_ci',
+  `api_name` VARCHAR(64) NOT NULL COMMENT '接口名称' COLLATE 'utf8_general_ci',
+  `api_params` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+  `api_method` VARCHAR(8) NOT NULL COLLATE 'utf8_general_ci',
+  `api_response_struct` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+  `mock_response_data` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+  `update_user` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+  `update_at` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `doc_api_path_uindex` (`doc_id`, `api_path`) USING BTREE,
+  INDEX `group_key` (`group_key`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
