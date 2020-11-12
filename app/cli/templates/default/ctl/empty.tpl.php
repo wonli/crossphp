e722e4bfdf4364f81db3a18a8e529343c953e687
ideaa <ideaa@qq.com>
2020-11-12 14:41:14 +0800

    修改生成模板

--- app/cli/templates/default/ctl/empty.tpl.php
+++ app/cli/templates/default/ctl/empty.tpl.php
@@ -1,6 +1,4 @@
-<div style="font-size:20px">
-    模板路径: <?= str_replace(PROJECT_REAL_PATH, '', $data['path'] ?? '') ?>
-</div>
-<div>
-    <?= date('Y-m-d H:i:s') ?>
-</div>
+<div style="font-size:20px">模板路径: <?= str_replace(PROJECT_REAL_PATH,
+        '', $data['path'] ?? '') ?></div>
+<div>生成时间: <?= date('Y-m-d H:i:s') ?></div>
+<pre><?= '<?= var_export($data ?? [], true) ?>' ?></pre>
