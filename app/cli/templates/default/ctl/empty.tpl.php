6946f83e71eb66c2b9dfadc5168343074d9c146d
ideaa <ideaa@qq.com>
2020-11-12 14:43:13 +0800

    *

--- app/cli/templates/default/ctl/empty.tpl.php
+++ app/cli/templates/default/ctl/empty.tpl.php
@@ -1,4 +1,4 @@
 <div style="font-size:20px">模板路径: <?= str_replace(PROJECT_REAL_PATH,
         '', $data['path'] ?? '') ?></div>
 <div>生成时间: <?= date('Y-m-d H:i:s') ?></div>
-<pre><?= '<?= var_export($data ?? [], true) ?>' ?></pre>
+<pre><?= '<?= print_r($data ?? [], true) ?>' ?></pre>
